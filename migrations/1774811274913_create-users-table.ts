e88d019a32f8addb7f3193f4e11457adacdfd179
marbedc <marbellacarrasco324@gmail.com>
2026-03-29 17:52:20 -0700

    m6-m7. Completed database foundation for m6-m7: created migration files for users and session tables

---
+++ migrations/1774811274913_create-users-table.ts
@@ -0,0 +1,17 @@
+import { ColumnDefinitions, MigrationBuilder } from 'node-pg-migrate';
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.createTable('users', {
+  id: 'id',
+  email: { type: 'varchar(255)', notNull: true , unique: true},
+  password_hash: { type: 'varchar(255)', notNull: true },
+  display_name: { type: 'varchar(50)', notNull: true },
+  created_at: { type: 'timestamp', notNull: true, default: pgm.func('current_timestamp') }
+});
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+    pgm.dropTable('users')
+}
